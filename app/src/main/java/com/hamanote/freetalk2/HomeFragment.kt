f5edc9780598f2cf5cbe35f6116cd46eb51a2bbe
hamani97 <hamani@naver.com>
2019-06-24 00:10:09 +0900

    - note

--- app/src/main/java/com/hamanote/freetalk2/HomeFragment.kt
+++ app/src/main/java/com/hamanote/freetalk2/HomeFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hamanote.freetalk2.MainActivity
 import com.hamanote.freetalk2.R
 import com.hamanote.freetalk2.base.BaseFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+    override fun onSelected() {
+//        activity.setTitle(R.string.app_name)
+        (activity as MainActivity).toolbar.title = "BBB"
+    }
     override fun initViews() {
+        (activity as MainActivity).toolbar.title = "BBB"
     }
 }
